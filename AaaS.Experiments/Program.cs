b0ba684113242d5e2e77e1f9a3ee2fd909711adb
Georg Edlbauer <edlbauerg@gmail.com>
2021-11-06 15:54:14 +0100

    added first db select attempts for detectors

--- AaaS.Experiments/Program.cs
+++ AaaS.Experiments/Program.cs
@@ -1,4 +1,5 @@
-﻿using AaaS.Dal.Ado;
+﻿using AaaS.Common;
+using AaaS.Dal.Ado;
 using System;
 
 namespace AaaS.Experiments
@@ -7,12 +8,10 @@ namespace AaaS.Experiments
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(new SimpleDetector().GetType().AssemblyQualifiedName);
-            var detector = AdoDetectorDao.MapRowToDetector(GetReturnRow());
-        }
-        private static MockDataRecord GetReturnRow()
-        {
-            return new MockDataRecord();
+            var config = ConfigurationUtil.GetConfiguration();
+            var factory = DefaultConnectionFactory.FromConfiguration(config, "PersonDbConnection");
+            var detector = new MSSQLDetectorDao(factory);
+            Console.WriteLine(detector);
         }
     }
 }
