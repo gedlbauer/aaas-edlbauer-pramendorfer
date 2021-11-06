50cbd566b51c0e8826f874dff488326748cc4834
Andreas Pramendorfer <1praand1@gmail.com>
2021-11-06 15:52:35 +0100

    Add client  dao test

---
+++ AaaS.Dal.Tests/ClientTests.cs
@@ -0,0 +1,33 @@
+﻿using AaaS.Dal.Ado;
+using AaaS.Dal.Interface;
+using AaaS.Domain;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AaaS.Dal.Tests
+{
+    public class ClientTests : IClassFixture<DatabaseFixture>
+    {
+        readonly DatabaseFixture fixture;
+        readonly IClientDao clientDao;
+
+        public ClientTests(DatabaseFixture fixture)
+        {
+            this.fixture = fixture;
+            clientDao = new MSSQLClientDao(this.fixture.ConnectionFactory);
+        }
+
+        [Fact]
+        public async Task TestInsert()
+        {
+            Client client = new Client { ApiKey = "test-keysdfd", Name = "test-user" };
+            await clientDao.InsertAsync(client);
+            (await clientDao.FindByIdAsync(client.Id)).Should().BeEquivalentTo(client);
+        }
+    }
+}
---
+++ AaaS.Dal.Tests/TestConnectionFactory.cs
@@ -0,0 +1,32 @@
+﻿using AaaS.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AaaS.Dal.Tests
+{
+    public class TestConnectionFactory : IConnectionFactory
+    {
+        private DbProviderFactory dbProviderFactory;
+
+        public string ConnectionString => "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\AaaSDb.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public string ProviderName => "Microsoft.Data.SqlClient";
+
+        public TestConnectionFactory()
+        {
+            DbUtil.RegisterAdoProviders();
+            this.dbProviderFactory = DbProviderFactories.GetFactory(ProviderName);
+        }
+        public async Task<DbConnection> CreateConnectionAsync()
+        {
+            DbConnection connection = dbProviderFactory.CreateConnection();
+            connection.ConnectionString = ConnectionString;
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
