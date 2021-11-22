7d9dea867f5f569238c07ce1201b6d0a05b67004
Georg Edlbauer <edlbauerg@gmail.com>
2021-11-22 19:06:01 +0100

    added seederclient

---
+++ AaaS.SeederClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace AaaS.SeederClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DestroyDatabase();
+        }
+
+        private static void DestroyDatabase()
+        {
+            string[] data = File.ReadAllLines(@"C:\temp\AaaS\seeding\bulk_insert_clients.sql");
+            foreach (var line in data)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
