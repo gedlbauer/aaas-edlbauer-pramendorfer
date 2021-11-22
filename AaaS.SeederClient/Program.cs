36a6cb85634408cc34a951dce58f1d3889f5eb7b
Georg Edlbauer <edlbauerg@gmail.com>
2021-11-22 19:27:12 +0100

    basic seeder functionality

--- AaaS.SeederClient/Program.cs
+++ AaaS.SeederClient/Program.cs
@@ -5,19 +5,10 @@ namespace AaaS.SeederClient
 {
     class Program
     {
+        private readonly string BASE_PATH = @"C:\temp\AaaS\seeding\";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            DestroyDatabase();
-        }
-
-        private static void DestroyDatabase()
-        {
-            string[] data = File.ReadAllLines(@"C:\temp\AaaS\seeding\bulk_insert_clients.sql");
-            foreach (var line in data)
-            {
-                Console.WriteLine(line);
-            }
         }
     }
 }
---
+++ AaaS.SeederClient/Seeder.cs
@@ -0,0 +1,48 @@
+﻿using AaaS.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AaaS.SeederClient
+{
+    public class Seeder
+    {
+        private string basePath;
+        private IConnectionFactory connectionFactory;
+
+        public Seeder(IConnectionFactory connectionFactory, string basePath)
+        {
+            this.basePath = basePath;
+            this.connectionFactory = connectionFactory;
+        }
+
+        public async Task DestroyDatabase()
+        {
+            await ExecuteScript(@"DbScripts\drop_database.sql");
+        }
+
+        public async Task CreateDatabase()
+        {
+            await ExecuteScript(@"DbScripts\create_datbase.ddl");
+        }
+
+        public async Task RecreateDatabase()
+        {
+            await DestroyDatabase();
+            await CreateDatabase();
+        }
+
+        private async Task ExecuteScript(string filepath)
+        {
+            string seedScript = await File.ReadAllTextAsync(filepath);
+            await using DbConnection connection = await connectionFactory.CreateConnectionAsync();
+            await using DbCommand command = connection.CreateCommand();
+            command.CommandText = seedScript;
+            command.ExecuteNonQuery();
+        }
+    }
+}
