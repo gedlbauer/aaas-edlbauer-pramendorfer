9538e91bb559df09263d52cc13416dec08aa8d27
Andreas Pramendorfer <1praand1@gmail.com>
2021-11-23 15:33:01 +0100

    Readme updates

--- AaaS.Dal.Ado/DbScripts/SeedData/bulk_insert_clients.sql
+++
@@ -1,8 +0,0 @@
-﻿BULK INSERT Client
-FROM 'C:\Users\edlba\source\repos\SWK\vz-aaas-g2-edlbauer-g2-pramendorfer\AaaS.Dal.Ado\DbScripts\SeedData\clients.csv'
-WITH (
-FIRSTROW=2,
-FIELDTERMINATOR = ',',
-ROWTERMINATOR = '0x0a'
-)
-GO
