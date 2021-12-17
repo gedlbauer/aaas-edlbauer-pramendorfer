1d27a2ac7c3a1eb055b8efe86a63bd43333f5ea7
Georg Edlbauer <edlbauerg@gmail.com>
2021-12-17 11:19:23 +0100

    added minimum detect interval and check timewindow

--- AaaS.Api/Dtos/Detector/DetectorInsertBaseDto.cs
+++ AaaS.Api/Dtos/Detector/DetectorInsertBaseDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AaaS.Api.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace AaaS.Api.Dtos.Detector
     {
         public int ActionId { get; set; }
         public string TelemetryName { get; set; }
+
+        [MinDuration(500)]
         public TimeSpan CheckInterval { get; set; }
     }
 }
--- AaaS.Api/Dtos/Detector/SlidingWindowDetectorInsertDto.cs
+++ AaaS.Api/Dtos/Detector/SlidingWindowDetectorInsertDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AaaS.Api.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ namespace AaaS.Api.Dtos.Detector
 {
     public class SlidingWindowDetectorInsertDto : DetectorInsertBaseDto
     {
+        [MinDuration(1000)]
         public TimeSpan TimeWindow { get; set; }
         public bool UseGreater { get; set; }
         public int Threshold { get; set; }
